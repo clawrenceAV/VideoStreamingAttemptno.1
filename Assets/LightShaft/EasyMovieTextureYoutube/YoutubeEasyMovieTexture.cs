902e50670a1c254ac4597d7b88c07caef3684320
Caleb <clawrence@avantagebrand.com>
2018-05-29 13:38:05 -0500

    Instantiation
    good luck jeff

---
+++ Assets/LightShaft/EasyMovieTextureYoutube/YoutubeEasyMovieTexture.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using YoutubeLight;
+using System.Collections.Generic;
+
+public class YoutubeEasyMovieTexture : MonoBehaviour
+{
+    public string videoId = "";
+    private string videoUrl;
+    RequestResolver resolver;
+
+    public void Start()
+    {
+        resolver = gameObject.AddComponent<RequestResolver>();
+        GetURL(videoId);
+    }
+
+    public void GetURL(string _videoId)
+    {
+        videoId = _videoId;
+        StartCoroutine(resolver.GetDownloadUrls(FinishLoadingUrls, videoId, false));
+    }
+
+    void FinishLoadingUrls()
+    {
+        Debug.Log("ok");
+        List<VideoInfo> videoInfos = resolver.videoInfos;
+        foreach (VideoInfo info in videoInfos)
+        {
+            if (info.VideoType == VideoType.Mp4 && info.Resolution == (720))
+            {
+                if (info.RequiresDecryption)
+                {
+                    //The string is the video url
+                    Debug.Log("Decript");
+                    StartCoroutine(resolver.DecryptDownloadUrl(DecryptionFinished, info));
+                    break;
+                }
+                else
+                {
+                    videoUrl = info.DownloadUrl;
+                    this.gameObject.GetComponent<MediaPlayerCtrl>().m_strFileName = videoUrl;
+                    this.gameObject.GetComponent<MediaPlayerCtrl>().Play();
+                }
+                break;
+            }
+        }
+    }
+
+    public void DecryptionFinished(string url)
+    {
+        videoUrl = url;
+        /*
+         *  IF YOU HAVE EASY MOVIE TEXTURE, ADD THIS SCRIPT IN THE SAME GAME OBJECT AS THE "MediaPlayerCtrl" ARE
+         *  Then uncomment these lines below.
+         */
+        this.gameObject.GetComponent<MediaPlayerCtrl>().m_strFileName = videoUrl;
+        this.gameObject.GetComponent<MediaPlayerCtrl>().Play(); //---> or equivalent if the MediaPlayerCtrl is not setted to play auto at start.
+    }
+}
---
+++ Assets/LightShaft/Scripts/YoutubeLivestreamSystem_EMT.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using SimpleJSON;
+using System.Text;
+using System;
+
+public class YoutubeLivestreamSystem_EMT : MonoBehaviour
+{
+    /*PRIVATE INFO DO NOT CHANGE THESE URLS OR VALUES*/
+    private const string serverURI = "https://unity-dev-youtube.herokuapp.com/api/info?url=https://www.youtube.com/watch?v=";
+    private const string formatURI = "&format=best&flatten=true";
+    /*END OF PRIVATE INFO*/
+    [Header("Easy movie texture MediaPlayerCtrl componet")]
+    public MediaPlayerCtrl easyPlayer;
+
+    [SerializeField]
+    public YoutubeLive liveResults;
+
+    public VideoQuality videoQuality;
+
+    public string videoId = "RtU_mdL2vBM";
+    private string videoUrl;
+    //start playing the video
+    public bool playOnStart = false;
+
+    public void Start()
+    {
+        if (playOnStart)
+        {
+            PlayYoutubeVideo(videoId);
+        }
+    }
+
+    public void PlayYoutubeVideo(string _videoId)
+    {
+        videoId = _videoId;
+        StartCoroutine(LiveRequest(videoId));
+    }
+
+    IEnumerator LiveRequest(string videoID)
+    {
+        WWW request = new WWW(serverURI + "" + videoID + "" + formatURI);
+        Debug.Log("FO");
+        yield return request;
+        var requestData = JSON.Parse(request.text);
+        var videos = requestData["videos"][0]["formats"];
+
+        for (int counter = 0; counter < videos.Count; counter++)
+        {
+            if (videos[counter]["format_id"] == "93")
+            {
+                liveResults.lowQuality = videos[counter]["manifest_url"];    //360
+            }
+            else if (videos[counter]["format_id"] == "95" || videos[counter]["format_id"] == "300")//300
+            {
+                liveResults.mediumQuality = videos[counter]["manifest_url"];  //720p
+            }
+            else if (videos[counter]["format_id"] == "96" || videos[counter]["format_id"] == "301")//301
+            {
+                liveResults.fullHdQuality = videos[counter]["manifest_url"];  //1080p
+            }
+            else if (videos[counter]["format_id"] == "97")
+            {
+                liveResults.ultraHdQuality = videos[counter]["manifest_url"];  //@2160p 4k
+            }
+
+        }
+
+        StartCoroutine(LivePlay());
+    }
+
+
+    IEnumerator LivePlay() //The prepare not respond so, i forced to play after some seconds
+    {
+        yield return new WaitForSeconds(0.5f);
+        string uri = "";
+        switch (videoQuality)
+        {
+            case VideoQuality.LowQuality:
+                uri = liveResults.lowQuality;
+                break;
+            case VideoQuality.Hd720:
+                uri = liveResults.mediumQuality;
+                break;
+            case VideoQuality.Hd1080:
+                uri = liveResults.fullHdQuality;
+                break;
+            case VideoQuality.Hd2160:
+                uri = liveResults.ultraHdQuality;
+                break;
+        }
+        easyPlayer.m_strFileName = uri;
+        StartCoroutine(Play());
+
+
+    }
+
+    public IEnumerator Play()
+    {
+        yield return new WaitForSeconds(4);
+        easyPlayer.Play();
+    }
+
+    public enum VideoQuality
+    {
+        LowQuality,
+        Hd720,
+        Hd1080,
+        Hd2160
+    }
+
+
+    public int GetMaxQualitySupportedByDevice()
+    {
+        if (Screen.orientation == ScreenOrientation.Landscape)
+        {
+            //use the height
+            return Screen.currentResolution.height;
+        }
+        else if (Screen.orientation == ScreenOrientation.Portrait)
+        {
+            //use the width
+            return Screen.currentResolution.width;
+        }
+        else
+        {
+            return Screen.currentResolution.height;
+        }
+    }
+}
+
+[System.Serializable]
+public class YoutubeLive
+{
+    public string lowQuality;
+    public string mediumQuality;
+    public string fullHdQuality;
+    public string ultraHdQuality;
+}
